368fefc79f0a448d57175ed5ea6579dd63ba1c3a
Clytra <48762967+Clytra@users.noreply.github.com>
2020-12-06 19:15:13 +0100

    Dodanie projektu - gra FizzBuzz

---
+++ Week3/FizzBuzzGame/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzzGame
+{
+    public class FizzBuzz
+    {
+        public void CheckNumber(int value)
+        {
+            if (value % 3 == 0 && value % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+                Console.ReadLine();
+            }
+            else if (value % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+                Console.ReadLine();
+            }
+            else if (value % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(value.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Week3/FizzBuzzGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzzGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Gra FizzBuzz. Podaj liczbę:");
+                var number = GetValue();
+
+                FizzBuzz fizzBuzz = new FizzBuzz();
+                fizzBuzz.CheckNumber(number);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+        private static int GetValue()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int value))
+                throw new Exception("Nieprawidłowe dane wejściowe!");
+
+            return value;
+        }
+    }
+}
