b1fb5389abc2f681caab2872d0a618a5c75b5482
Clytra <48762967+Clytra@users.noreply.github.com>
2020-12-21 21:05:11 +0100

    Dodanie klasy pracownika

---
+++ DesktopApp/HrApp/HrApp/HrApp/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HrApp
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfEmployment { get; set; }
+        public DateTime DateOfEmployeeDismissal { get; set; }
+        public int EmployeeNumber { get; set; }
+        public string Comments { get; set; }
+        public double Earnings { get; set; }
+    }
+}
