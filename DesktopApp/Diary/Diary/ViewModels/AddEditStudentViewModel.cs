8313a47b386fce0bb409df62e5aeb8ee88dc5a41
Clytra <48762967+Clytra@users.noreply.github.com>
2021-01-24 22:30:22 +0100

    Logika aplikacji

--- DesktopApp/Diary/Diary/ViewModels/AddEditStudentViewModel.cs
+++ DesktopApp/Diary/Diary/ViewModels/AddEditStudentViewModel.cs
@@ -1,6 +1,80 @@
-﻿namespace Diary.ViewModels
+﻿using Diary.Commands;
+using Diary.Models;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Diary.ViewModels
 {
     public class AddEditStudentViewModel : ViewModelBase
     {
+        public AddEditStudentViewModel(Student student = null)
+        {
+            CloseCommand = new RelayCommand(Close);
+            ConfirmCommand = new RelayCommand(Confirm);
+
+            if (student == null)
+            {
+                Student = new Student();
+            }
+            else
+            {
+                Student = student;
+                IsUpdate = true;
+            }
+        }
+        public ICommand CloseCommand { get; set; }
+        public ICommand ConfirmCommand { get; set; }
+
+        private Student _student;
+        public Student Student
+        {
+            get { return _student; }
+            set
+            {
+                _student = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isUpdate;
+        public bool IsUpdate
+        {
+            get { return _isUpdate; }
+            set
+            {
+                _isUpdate = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private void Confirm(object obj)
+        {
+            if (!IsUpdate)
+                AddStudent();
+            else
+                UpdateStudent();
+
+            CloseWindow(obj as Window);
+        }
+
+        private void UpdateStudent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddStudent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Close(object obj)
+        {
+            CloseWindow(obj as Window);
+        }
+        private void CloseWindow(Window window)
+        {
+            window.Close();
+        }
     }
 }
--- DesktopApp/Diary/Diary/ViewModels/MainViewModel.cs
+++ DesktopApp/Diary/Diary/ViewModels/MainViewModel.cs
@@ -1,4 +1,11 @@
 ﻿using Diary.Commands;
+using Diary.Models;
+using Diary.Views;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -9,12 +16,102 @@ namespace Diary.ViewModels
         public MainViewModel()
         {
             RefreshStudentsCommand = new RelayCommand(RefreshStudents);
+            AddStudentCommand = new RelayCommand(AddEditStudent);
+            EditStudentCommand = new RelayCommand(AddEditStudent, CanEditDeleteStudent);
+            DeleteStudentCommand = new AsyncRelayCommand(DeleteStudent, CanEditDeleteStudent);
+
+            InitGroups();
         }
+
         public ICommand RefreshStudentsCommand { get; set; }
+        public ICommand AddStudentCommand { get; set; }
+        public ICommand EditStudentCommand { get; set; }
+        public ICommand DeleteStudentCommand { get; set; }
+
+        private Student _selectedStudent;
+        public Student SelectedStudent
+        {
+            get { return _selectedStudent; }
+            set
+            {
+                _selectedStudent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Student> _students;
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            set
+            {
+                _students = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Group _selectedGroupId;
+        public Group SelectedGroupId
+        {
+            get { return _selectedGroupId; }
+            set
+            {
+                _selectedGroupId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Group> _groups;
+        public ObservableCollection<Group> Groups
+        {
+            get { return _groups; }
+            set
+            {
+                _groups = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool CanEditDeleteStudent(object obj)
+        {
+            return SelectedStudent != null;
+        }
+        private async Task DeleteStudent(object obj)
+        {
+            var metroWindow = Application.Current.MainWindow as MetroWindow;
+            var dialog = await metroWindow.ShowMessageAsync(
+                "Usuwanie ucznia", 
+                $"Czy na pewno chcesz usunąć ucznia {SelectedStudent.FisrtName}" +
+                $" {SelectedStudent.LastName}?", 
+                MessageDialogStyle.AffirmativeAndNegative);
+
+            if (dialog != MessageDialogResult.Affirmative)
+            {
+                return;
+
+                RefreshDiary();
+            }
+        }
+
+        private void AddEditStudent(object obj)
+        {
+            var addEditStudentWindow = new AddEditStudentView(obj as Student);
+            addEditStudentWindow.Closed += addEditStudentWindow_Closed;
+            addEditStudentWindow.ShowDialog();
+        }
+
+        private void addEditStudentWindow_Closed(object sender, EventArgs e)
+        {
+            RefreshDiary();
+        }
 
         private void RefreshStudents(object obj)
         {
-            MessageBox.Show("RefreshStudents");
+            RefreshDiary();
+        }
+
+        private void InitGroups()
+        {
         }
     }
 }
