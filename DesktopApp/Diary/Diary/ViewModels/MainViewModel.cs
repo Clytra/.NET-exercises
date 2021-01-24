fbb802a1f374f0ae28720fe583bbad5dc1469395
Clytra <48762967+Clytra@users.noreply.github.com>
2021-01-24 17:10:05 +0100

    Szkielet MVVM

---
+++ DesktopApp/Diary/Diary/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Diary.Commands;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Diary.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            RefreshStudentsCommand = new RelayCommand(RefreshStudents);
+        }
+        public ICommand RefreshStudentsCommand { get; set; }
+
+        private void RefreshStudents(object obj)
+        {
+            MessageBox.Show("RefreshStudents");
+        }
+    }
+}
