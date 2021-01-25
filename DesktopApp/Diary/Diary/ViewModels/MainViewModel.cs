214406593b2d94e5894f5f3e9623e7ace57fae50
Clytra <48762967+Clytra@users.noreply.github.com>
2021-01-25 22:19:16 +0100

    Wstępne bindowane pierwszego przycisku

--- DesktopApp/Diary/Diary/ViewModels/MainViewModel.cs
+++ DesktopApp/Diary/Diary/ViewModels/MainViewModel.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Diary.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Diary.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        public MainViewModel()
+        {
+            RefreshStudentsCommand = new RelayCommand(RefreshStudents, CanRefreshStudents);
+        }
+
+        public ICommand RefreshStudentsCommand { get; set; }
+
+        private bool CanRefreshStudents(object obj)
+        {
+            return true;
+        }
+
+        private void RefreshStudents(object obj)
+        {
+            MessageBox.Show("RefreshStudent");
+        }
     }
 }
