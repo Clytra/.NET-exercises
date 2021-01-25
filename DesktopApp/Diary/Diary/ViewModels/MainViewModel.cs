de68dbad1afbc26ceed33501716bbe0b87264b43
Clytra <48762967+Clytra@users.noreply.github.com>
2021-01-25 22:35:58 +0100

    Dodanie modeli Student i Group + wstępne dane

--- DesktopApp/Diary/Diary/ViewModels/MainViewModel.cs
+++ DesktopApp/Diary/Diary/ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Diary.Commands;
+using Diary.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +16,55 @@ namespace Diary.ViewModels
         public MainViewModel()
         {
             RefreshStudentsCommand = new RelayCommand(RefreshStudents, CanRefreshStudents);
+
+            Students = new ObservableCollection<Student>
+            {
+                new Student
+                {
+                    FirstName ="Janusz",
+                    LastName="Januszowy",
+                    Group = new Group { Id = 1}
+                },
+                new Student
+                {
+                    FirstName ="Marek",
+                    LastName="Januszowy",
+                    Group = new Group { Id = 2}
+                },
+                new Student
+                {
+                    FirstName ="Grażyna",
+                    LastName="Januszowa",
+                    Group = new Group { Id = 3}
+                },
+            };
         }
 
         public ICommand RefreshStudentsCommand { get; set; }
+
+        private ObservableCollection<Student> _students;
+
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            set
+            {
+                _students = value;
+                OnPropertyChanged();
+            }
+        }
+        private Student _selectedStudent;
+
+        public Student SelectedStudent
+        {
+            get { return _selectedStudent; }
+            set 
+            { 
+                _selectedStudent = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         private bool CanRefreshStudents(object obj)
         {
