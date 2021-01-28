4a9e0bc39b69a6e9a46baa5ffb4a522f892e69aa
Clytra <48762967+Clytra@users.noreply.github.com>
2021-01-28 14:05:41 +0100

    Dodanie klasy domenowej przechowującej dane do ConnectionStringa

---
+++ DesktopApp/Diary/Diary/Models/Domains/Server.cs
@@ -0,0 +1,16 @@
+﻿namespace Diary.Models.Domains
+{
+    public class Server
+    {
+        public Server()
+        {
+
+        }
+
+        public string ServerAddress { get; set; }
+        public string ServerName { get; set; }
+        public string DbName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
