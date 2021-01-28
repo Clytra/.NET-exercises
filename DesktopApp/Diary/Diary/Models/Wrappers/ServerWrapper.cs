7c4ffc192a00ef2d8a7f91a48578ce3f5c21e2ee
Clytra <48762967+Clytra@users.noreply.github.com>
2021-01-28 14:26:17 +0100

    Dodanie wrappera do danych ustawień z walidacją

---
+++ DesktopApp/Diary/Diary/Models/Wrappers/ServerWrapper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diary.Models.Wrappers 
+{
+    public class ServerWrapper : IDataErrorInfo
+    {
+        public string ServerAddress { get; set; }
+        public string ServerName { get; set; }
+        public string DbName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        private bool _isServerAddressValid;
+        private bool _isServerNameValid;
+        private bool _isDbNameValid;
+        private bool _isUserNameValid;
+        private bool _isPasswordValid;
+
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case nameof(ServerAddress):
+                        if (string.IsNullOrWhiteSpace(ServerAddress))
+                        {
+                            Error = "Pole Adres Serwera jest wymagane.";
+                            _isServerAddressValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isServerAddressValid = true;
+                        }
+                        break;
+                    case nameof(ServerName):
+                        if (string.IsNullOrWhiteSpace(ServerName))
+                        {
+                            Error = "Pole Nazwa Serwera jest wymagane.";
+                            _isServerNameValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isServerNameValid = true;
+                        }
+                        break;
+                    case nameof(DbName):
+                        if (string.IsNullOrWhiteSpace(DbName))
+                        {
+                            Error = "Pole Nazwa Bazy Danych jest wymagane.";
+                            _isDbNameValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isDbNameValid = true;
+                        }
+                        break;
+                    case nameof(UserName):
+                        if (string.IsNullOrWhiteSpace(UserName))
+                        {
+                            Error = "Pole Nazwa Użytkownika jest wymagane.";
+                            _isUserNameValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isUserNameValid = true;
+                        }
+                        break;
+                    case nameof(Password):
+                        if (string.IsNullOrWhiteSpace(Password))
+                        {
+                            Error = "Pole Hasło jest wymagane.";
+                            _isPasswordValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isPasswordValid = true;
+                        }
+                        break;
+                }
+
+                return Error;
+            }
+        }
+        public string Error { get; set; }
+    }
+}
