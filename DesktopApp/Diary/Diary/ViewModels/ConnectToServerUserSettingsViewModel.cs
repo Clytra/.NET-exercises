aac76480ede4867a202b3b71f2dd4a15308178c0
Clytra <48762967+Clytra@users.noreply.github.com>
2021-01-28 14:14:11 +0100

    Wstępne przygotowanie modelu widoku do powiązania danych

--- DesktopApp/Diary/Diary/ViewModels/ConnectToServerUserSettingsViewModel.cs
+++ DesktopApp/Diary/Diary/ViewModels/ConnectToServerUserSettingsViewModel.cs
@@ -1,9 +1,7 @@
-﻿using Diary.Models.Domains;
+﻿using Diary.Commands;
+using Diary.Models.Domains;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Diary.ViewModels
 {
@@ -11,7 +9,33 @@ namespace Diary.ViewModels
     {
         public ConnectToServerUserSettingsViewModel(Server server = null)
         {
+            CloseCommand = new RelayCommand(Close);
+            ConfirmCommand = new RelayCommand(Confirm);
+        }
 
+        public ICommand CloseCommand { get; set; }
+        public ICommand ConfirmCommand { get; set; }
+
+
+        private Server _server;
+        public Server Server
+        {
+            get { return _server; }
+            set
+            {
+                _server = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void Confirm(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Close(object obj)
+        {
+            throw new NotImplementedException();
         }
     }
 }
