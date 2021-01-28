387e176a140f1144ebefdf2a3bacd636633a51c0
Clytra <48762967+Clytra@users.noreply.github.com>
2021-01-28 16:06:18 +0100

    Dodanie enum'a ze statusami połączenia

---
+++ DesktopApp/Diary/Diary/Models/ConnectionStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Diary.Models
+{
+    public enum ConnectionStatus
+    {
+        CONNECTION_SUCCESSFUL = 1,
+        ERROR_CONNECTION,
+        USER_ATTEMPTING_TO_CONNECT,
+        CONNECTION_CANCELLED_BY_USER
+    }
+}
