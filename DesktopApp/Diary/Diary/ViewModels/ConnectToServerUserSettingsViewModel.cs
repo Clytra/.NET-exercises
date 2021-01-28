74b9765fc1aa586c6b599120e947c5cbdcfaca37
Clytra <48762967+Clytra@users.noreply.github.com>
2021-01-28 14:05:59 +0100

    Dodanie klasy ViewModel do ustawień

---
+++ DesktopApp/Diary/Diary/ViewModels/ConnectToServerUserSettingsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Diary.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diary.ViewModels
+{
+    public class ConnectToServerUserSettingsViewModel : ViewModelBase
+    {
+        public ConnectToServerUserSettingsViewModel(Server server = null)
+        {
+
+        }
+    }
+}
