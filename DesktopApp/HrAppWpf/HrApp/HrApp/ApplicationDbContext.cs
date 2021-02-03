b7832b5040676bb749639bcb876f5068512a909e
Clytra <48762967+Clytra@users.noreply.github.com>
2021-02-03 11:38:40 +0100

    Instalacja Entity Framework

---
+++ DesktopApp/HrAppWpf/HrApp/HrApp/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace HrApp
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("name=ApplicationDbContext") { }
+    }
+}
