7d4734fdf185fa7f19f312adf17c48b171d9cb31
Clytra <48762967+Clytra@users.noreply.github.com>
2021-02-08 21:18:48 +0100

    Dodanie obsługi przykładowych parametrów

--- Service/ReportService/ReportService/Program.cs
+++ Service/ReportService/ReportService/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration.Install;
+using System.Reflection;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReportService
 {
@@ -12,8 +10,24 @@ namespace ReportService
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
+            if (Environment.UserInteractive)
+            {
+                var parameter = string.Concat(args);
+                switch (parameter)
+                {
+                    case "--install":
+                        ManagedInstallerClass.InstallHelper(new[]
+                        { Assembly.GetExecutingAssembly().Location });
+                        break;
+                    case "--uninstall":
+                        ManagedInstallerClass.InstallHelper(new[] {  "/u",
+                        Assembly.GetExecutingAssembly().Location });
+                        break;
+                }
+            }
+
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
--- Service/ReportService/ReportService/ProjectInstaller.Designer.cs
+++ Service/ReportService/ReportService/ProjectInstaller.Designer.cs
@@ -29,9 +29,32 @@ namespace ReportService
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalService;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "Wysyłanie raportów";
+            this.serviceInstaller1.DisplayName = "ReportService";
+            this.serviceInstaller1.ServiceName = "ReportService";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
         }
 
         #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
     }
 }
