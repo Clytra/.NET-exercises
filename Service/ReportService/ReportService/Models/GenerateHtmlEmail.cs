a7845691a8fbe185b8192734dd77bbf0e50c9e05
Clytra <48762967+Clytra@users.noreply.github.com>
2021-02-10 13:16:21 +0100

    Dodanie metody generującej html dla raportu

--- Service/ReportService/ReportService/Models/GenerateHtmlEmail.cs
+++ Service/ReportService/ReportService/Models/GenerateHtmlEmail.cs
@@ -40,5 +40,44 @@ namespace ReportService.Models
 
             return html;
         }
+
+        public string GenerateReport(Report report)
+        {
+            if (report == null)
+                throw new ArgumentNullException(nameof(report));
+
+            var html = $"Raport {report.Title} z dnia {report.Date.ToString("dd-MM-yyyy")}.<br /><br />";
+
+            if (report.Positions != null && report.Positions.Any())
+            {
+                html +=
+                @"
+                    <table border=1 cellpadding=5 cellspacing=1>
+                        <tr>
+                            <td allign=center bgcolor=lightgrey>Tytuł</td>
+                            <td allign=center bgcolor=lightgrey>Opis</td>
+                            <td allign=center bgcolor=lightgrey>Wartość</td>
+                        </tr>
+                ";
+            }
+
+            foreach (var position in report.Positions)
+            {,
+                html +=
+                    $@"<tr>
+                        <td allign=center>{position.Title}</td>
+                        <td allign=center>{position.Description}</td>
+                        <td allign=center>{position.Value.ToString("0.00")} zł</td>
+                       </tr>
+                    ";
+            }
+
+            if (report.Positions != null && report.Positions.Any())
+                html += "</table>";
+            else
+                html += "-- Brak danych do wyświetlenia --";
+
+            html += @"<br /><br/ ><i>Automatyczna wiadomość wysłana w aplikacji ReportService.</i>";
+        }
     }
 }
