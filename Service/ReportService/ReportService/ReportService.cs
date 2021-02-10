796c620a9ac184ba52ef8d43e59ebb7fd70ff074
Clytra <48762967+Clytra@users.noreply.github.com>
2021-02-10 12:11:32 +0100

    Ustalenie timera wywołującego metodę

--- Service/ReportService/ReportService/ReportService.cs
+++ Service/ReportService/ReportService/ReportService.cs
@@ -7,14 +7,17 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace ReportService
 {
     public partial class ReportService : ServiceBase
     {
+        private const int IntervalInMinutes = 30;
+        private Timer _timer = new Timer(IntervalInMinutes * 60000);
         public ReportService()
         {
-            InitializeComponent();
+            InitializeComponent(); 
         }
 
         protected override void OnStart(string[] args)
