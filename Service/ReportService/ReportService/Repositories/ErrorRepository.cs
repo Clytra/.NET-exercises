95e11ef7613ef6d6b4fca36baa845f4ef1a0aab5
Clytra <48762967+Clytra@users.noreply.github.com>
2021-02-10 12:18:45 +0100

    Dodanie klasy ErrorRepository

---
+++ Service/ReportService/ReportService/Repositories/ErrorRepository.cs
@@ -0,0 +1,22 @@
+﻿using ReportService.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportService.Repositories
+{
+    public class ErrorRepository
+    {
+        public List<Error> GetLasError(int intervalInMinutes)
+        {
+            //pobieranie z bazy danych
+            return new List<Error>
+            {
+                new Error {Message = "Błąd tekstowy 1", Date = DateTime.Now},
+                new Error {Message = "Błąd tekstowy 2", Date = DateTime.Now}
+            };
+        }
+    }
+}
