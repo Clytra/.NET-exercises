948334a9fdbf55cd77488745358d64e87f064664
Clytra <48762967+Clytra@users.noreply.github.com>
2021-02-14 21:42:51 +0100

    Obsługa wysyłki maila

---
+++ Service/ReportService/ReportService/Models/Email.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+
+namespace ReportService.Models
+{
+    public class Email
+    {
+        private SmtpClient _smtp;
+        private MailMessage _mail;
+
+        private string _hostSmtp;
+        private bool _enableSsl;
+        private int _port;
+        private string _senderEmail;
+        private string _senderEmailPassword;
+        private string _senderName;
+
+        public Email(EmaiParams emaiParams)
+        {
+            _hostSmtp = emaiParams.HostSmtp;
+            _enableSsl = emaiParams.EnableSsl;
+            _port = emaiParams.Port;
+            _senderEmail = emaiParams.SenderEmail;
+            _senderEmailPassword = emaiParams.SenderEmailPassword;
+            _senderName = emaiParams.SenderName;
+        }
+
+        public void Send(string subject, string body, string to)
+        {
+            _mail = new MailMessage();
+            _mail.From = new MailAddress(_senderEmail, _senderName);
+            _mail.To.Add(new MailAddress(to));
+            _mail.IsBodyHtml = true;
+            _mail.Subject = subject;
+            _mail.BodyEncoding = Encoding.UTF8;
+            _mail.SubjectEncoding = Encoding.UTF8;
+
+            _mail.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(body,
+                null, MediaTypeNames.Text.Plain));
+        }
+    }
+}
