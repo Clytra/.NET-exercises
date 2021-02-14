7bb00cf2d36b8a7c082fca49b576ed97aad73221
Clytra <48762967+Clytra@users.noreply.github.com>
2021-02-14 21:45:35 +0100

    metoda usuwająca tagi html

---
+++ Service/ReportService/ReportService/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ReportService.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string StripHTML(this string input)
+        {
+            return Regex.Replace(input, "<.*?>", String.Empty);
+        }
+    }
+}
