ee0fee2c027f6d08dced8f5cf9da4b3880db0f6a
Clytra <48762967+Clytra@users.noreply.github.com>
2021-02-28 19:37:59 +0100

    Modyfikacja usługi na local service

--- Service/ReportErrorsService/ReportErrorsService/ProjectInstaller.Designer.cs
+++ Service/ReportErrorsService/ReportErrorsService/ProjectInstaller.Designer.cs
@@ -34,6 +34,7 @@ namespace ReportErrorsService
             // 
             // serviceProcessInstaller1
             // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalService;
             this.serviceProcessInstaller1.Password = null;
             this.serviceProcessInstaller1.Username = null;
             // 
