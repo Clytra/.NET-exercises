d77ea0fa77f3d35c7436433d3b999348acac5e84
Clytra <48762967+Clytra@users.noreply.github.com>
2021-03-15 22:31:51 +0100

    Uporządkowanie modelu Identity

--- WebApps/InvoiceManager/InvoiceManager/Models/Domains/ApplicationUser.cs
+++ WebApps/InvoiceManager/InvoiceManager/Models/Domains/ApplicationUser.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace InvoiceManager.Models.ViewModels
+namespace InvoiceManager.Models.Domains
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
@@ -14,19 +14,6 @@ namespace InvoiceManager.Models.ViewModels
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
-        }
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
         }
     }
 }
